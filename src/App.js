dd324e94ae262d581ca0c97bf9555a8794f90d81
Florian Möschler <florian.moeschler@outlook.de>
2020-06-07 17:05:00 +0200

    Dashboard im Anfangsstadium

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import Login from './pages/Login/login'
+import NavBar from './NavBar'
 import DashboardTemplate from './pages/dashboard/dashboardTemplate'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <DashboardTemplate/>
     </div>
   );
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import logo from './logo.svg';
 
 class NavBar extends React.Component{
 
     render(){
         return(
-            <
+            <table id="navbar">
+                <tr>
+                    <td><a><image href={logo}/></a></td>
+                    <td><a>+</a></td>
+                    <td><a>User</a></td>
+                    <td><a>Log</a></td>
+                </tr>
+            </table>
         );
     }
 
